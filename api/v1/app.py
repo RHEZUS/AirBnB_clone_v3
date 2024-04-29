cdf15ed83758a94f28c28eddc6cc30d797cf3989
RHEZUS <ludivinseunda2@gmail.com>
2024-04-29 17:41:03 +0300

    solve task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,9 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import make_response
+from flask import jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,6 +20,22 @@ def teardown_db(exception):
     """
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 if __name__ == '__main__':
     if getenv("HBNB_API_HOST") is None:
